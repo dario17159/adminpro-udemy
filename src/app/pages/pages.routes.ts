0d59660abc10045a71d8a5c176788c7d0a0701f5
Dario Carrizo <jose.2694.dc@gmail.com>
2019-03-26 00:48:23 -0300

    Seccion 17 terminada

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -6,12 +6,13 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
-import { LoginGuardGuard } from '../services/service.index';
+import { LoginGuardGuard, AdminGuard } from '../services/service.index';
 import { ProfileComponent } from './profile/profile.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { HospitalesComponent } from './hospitales/hospitales.component';
 import { MedicosComponent } from './medicos/medicos.component';
 import { MedicoComponent } from './medicos/medico.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 const pagesRoutes: Routes = [
   {
@@ -49,10 +50,16 @@ const pagesRoutes: Routes = [
         component: ProfileComponent,
         data: { titulo: 'Perfil de Usuario' }
       },
+      {
+        path: 'busqueda/:termino',
+        component: BusquedaComponent,
+        data: { titulo: 'Buscador' }
+      },
       // mantenimientos
       {
         path: 'usuarios',
         component: UsuariosComponent,
+        canActivate: [AdminGuard],
         data: { titulo: 'Mantenimiento de Usuarios' }
       },
       {
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -5,9 +5,10 @@ import { URL_SERVICIOS } from 'src/app/config/config';
 
 declare var swal;
 
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { SubirArchivoService } from '../subir-archivo/subir-archivo.service';
+import { throwError } from 'rxjs/internal/observable/throwError';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,7 @@ export class UsuarioService {
 
   usuario: Usuario;
   token: string;
+  menu: any[] = [];
 
   constructor( private http: HttpClient , private router: Router, private _subirArchivoService: SubirArchivoService) {
     this.cargarStorage();
@@ -37,9 +39,11 @@ export class UsuarioService {
     if(localStorage.getItem('token')){
       this.token = localStorage.getItem('token');
       this.usuario = JSON.parse(localStorage.getItem('usuario'));
+      this.menu = JSON.parse(localStorage.getItem("menu"));
     }else {
       this.token = '';
       this.usuario = null;
+      this.menu = null;
     }
   }
 
@@ -47,14 +51,16 @@ export class UsuarioService {
   //         guardar datos en el storage local
   //========================================================================
 
-  guardarStorage( id: string, token: string, usuario: Usuario) {
+  guardarStorage( id: string, token: string, usuario: Usuario, menu: any) {
 
     localStorage.setItem('id', id);
     localStorage.setItem('token', token);
     localStorage.setItem('usuario', JSON.stringify(usuario));
+    localStorage.setItem('menu', JSON.stringify(menu));
 
     this.usuario = usuario;
     this.token = token;
+    this.menu = menu;
   }
 
 
@@ -65,8 +71,10 @@ export class UsuarioService {
   logOut(){
       this.usuario = null;
       this.token = '';
+      this.menu = [];
       localStorage.removeItem('token');
       localStorage.removeItem('usuario');
+      localStorage.removeItem('menu');
       this.router.navigate(['/login']);
   }
 
@@ -79,7 +87,7 @@ export class UsuarioService {
 
     return this.http.post( url, { token })
       .pipe(map( (resp: any) => {
-        this.guardarStorage(resp.id, resp.token, resp.usuario );
+        this.guardarStorage(resp.id, resp.token, resp.usuario, resp.menu);
         return true;
       }));
   }
@@ -99,11 +107,12 @@ export class UsuarioService {
 
     return this.http.post(url, usuario)
       .pipe(map( (resp: any) => {
-
-        this.guardarStorage(resp.id, resp.token, resp.usuario );
-
+        this.guardarStorage(resp.id, resp.token, resp.usuario, resp.menu );
         return true;
-
+      }),
+      catchError(err => {
+        swal('Error en el login','Usuario o contraseña incorrecta','error');
+        return throwError(err);
       }));
   }
 
@@ -120,7 +129,11 @@ export class UsuarioService {
       .pipe(map((resp: any) => {
         swal('Usuario Creado', usuario.email, 'success');
         return resp.usuario;
-      }));
+      }),
+        catchError(err => {
+          swal(err.error.mensaje, err.error.errors.message, 'error');
+          return throwError(err);
+        }));
   }
 
   //========================================================================
@@ -138,13 +151,17 @@ export class UsuarioService {
 
         if( usuario._id === this.usuario._id){
           const usuarioDB : Usuario = resp.usuario;
-          this.guardarStorage(usuarioDB._id, this.token, usuarioDB);
+          this.guardarStorage(usuarioDB._id, this.token, usuarioDB, this.menu);
         }
 
         swal('Usuario actualizado', usuario.nombre, 'success');
 
         return true;
-      }));
+      }),
+        catchError(err => {
+          swal('Error en el login', 'Usuario o contraseña incorrecta', 'error');
+          return throwError(err);
+        }));
   }
 
   //========================================================================
@@ -157,7 +174,7 @@ export class UsuarioService {
         this.usuario.img = resp.usuario.img;
         swal('Imagen Actualizada', this.usuario.nombre, 'success');
 
-        this.guardarStorage(id, this.token, this.usuario);
+        this.guardarStorage(id, this.token, this.usuario, this.menu);
       })
       .catch( resp => {
         console.log(resp);
